5ced8085f66960f6b93a91678c9ba9cba1f208bc
seanastricker <sean.a.stricker@gmail.com>
2025-07-16 17:38:17 -0500

    Feat(ui): add background image and enhance character creation UI

--- gauntletsim/scripts/CharacterCreation.gd
+++ gauntletsim/scripts/CharacterCreation.gd
@@ -1,20 +1,25 @@
 # CharacterCreation.gd
 extends Control
 
-@onready var name_edit: LineEdit = $VBoxContainer/NameEdit
-@onready var start_button: Button = $VBoxContainer/StartButton
+@onready var name_edit: LineEdit = $CenterContainer/VBoxContainer/NameEdit
+@onready var start_button: Button = $CenterContainer/VBoxContainer/StartButton
+@onready var label: Label = $CenterContainer/VBoxContainer/Label
 
 func _ready():
-    start_button.pressed.connect(_on_start_button_pressed)
-    name_edit.grab_focus()
+	start_button.pressed.connect(_on_start_button_pressed)
+	name_edit.grab_focus()
+	
+	label.add_theme_font_size_override("font_size", 48)
+	name_edit.add_theme_font_size_override("font_size", 48)
+	start_button.add_theme_font_size_override("font_size", 48)
 
 func _on_start_button_pressed():
-    var player_name = name_edit.text
-    if player_name.strip_edges().is_empty():
-        # Maybe show an error label
-        print("Name cannot be empty")
-        return
+	var player_name = name_edit.text
+	if player_name.strip_edges().is_empty():
+		# Maybe show an error label
+		print("Name cannot be empty")
+		return
 
-    PlayerData.player_name = player_name
-    
-    get_tree().change_scene_to_file("res://scenes/Main.tscn") 
+	PlayerData.player_name = player_name
+	
+	get_tree().change_scene_to_file("res://scenes/Main.tscn")
